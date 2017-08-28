95f1063b1efeae66b2385fb488a8915529930b73
dmartinesmedseek <david.martines@medseek.com>
2017-08-28 10:41:36 -0500

    EMPO-15793 Format file.

--- injectable/logging.js
+++ injectable/logging.js
@@ -2,24 +2,24 @@
 
 module.exports = function logging(log4js, options) {
 
-    var config = options.log.config || {
-        appenders: [
-            { type: 'console' }
-        ],
-        levels: {
-            '[all]': options.log.level
-        },
-        replaceConsole: options.log.replaceConsole === true,
-    };
+	var config = options.log.config || {
+		appenders: [{
+			type: 'console'
+		}],
+		levels: {
+			'[all]': options.log.level
+		},
+		replaceConsole: options.log.replaceConsole === true,
+	};
 
-    var opts = {
-        reloadSecs: options.log.refresh
-    };
+	var opts = {
+		reloadSecs: options.log.refresh
+	};
 
-    log4js.configure(config, opts);
+	log4js.configure(config, opts);
 
-    var log = log4js.getLogger('microservice-crutch.logging');
-    log.debug('Configured log4js; config:', config, '\n opts:', opts);
+	var log = log4js.getLogger('microservice-crutch.logging');
+	log.debug('Configured log4js; config:', config, '\n opts:', opts);
 
-    return log4js;
+	return log4js;
 };
