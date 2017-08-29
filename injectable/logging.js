c722d2762cd772c68f4e9ffff47d91cc8558c0d4
dmartinesmedseek <david.martines@medseek.com>
2017-08-29 12:36:56 -0500

    EMPO-15793 Log using structured json layout.

--- injectable/logging.js
+++ injectable/logging.js
@@ -1,14 +1,61 @@
 'use strict';
+var path = require('path');
 
 module.exports = function logging(log4js, options) {
+
+  log4js.addLayout('ih-structured-json', function (config) {
+
+    var componentName = path.basename(process.mainModule.filename, '.js');
+
+    return function convertToStructuredJson(logEvent) {
+      var struct = {
+        timestamp: logEvent.startTime,
+        component: componentName,
+        level: logEvent.level.levelStr,
+        pid: logEvent.pid
+      };
+      var messageData = [];
+      logEvent.data.forEach(function (datum) {
+        if (isErrorObject(datum)) {
+          struct.message = datum.message;
+          messageData.push(datum.stack);
+        } else if (isMessageContext(datum)) {
+          struct.tracking = datum.properties.tracking;
+          struct.context = datum.routingKey;
+        } else {
+          messageData.push(datum);
+        }
+      });
+      if (!struct.context) {
+        struct.context = logEvent.categoryName;
+      }
+      if (!struct.tracking) {
+        struct.tracking = null;
+      }
+      struct.message = struct.message || messageData[0] || null;
+      struct.messageData = messageData;
+      return JSON.stringify(struct) + config.separator;
+    };
+
+    function isMessageContext(datum) {
+      return (typeof datum === 'object') &&
+        (datum.properties !== undefined);
+    }
+
+    function isErrorObject(datum) {
+      return (typeof datum === 'object') &&
+        (datum.message !== undefined) &&
+        (datum.stack !== undefined);
+    }
+  });
 
   var config = options.log.config || {
     appenders: {
       out: {
         type: 'stdout',
         layout: {
-          type: 'pattern',
-          pattern: '[%d] [%p] [%c] [tracking:%X{tracking}] %m%n'
+          type: 'ih-structured-json',
+          separator: ','
         }
       }
     },
